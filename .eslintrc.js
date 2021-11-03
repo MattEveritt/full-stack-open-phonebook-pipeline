fad2c426052ded2dbb6b0038486f04c4c8bad66a
MattEveritt <email@example.com>
2021-11-03 12:15:08 +0200

    package.json update
    Updating eslint configuration in package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,10 @@ module.exports = {
         'commonjs': true,
         'es2021': true
     },
-    'extends': 'eslint:recommended',
+    'extends': [
+        'eslint:recommended',
+        'react-app'
+    ],
     'parserOptions': {
         'ecmaVersion': 12
     },
