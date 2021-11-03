4df94c17ed6bd6aa900844d91ea8c6e39abbbe9f
MattEveritt <email@example.com>
2021-11-03 12:21:37 +0200

    fixed lint errors

--- client/src/components/Persons.js
+++ client/src/components/Persons.js
@@ -3,16 +3,25 @@ import React from 'react'
 const Persons = (props) => {
     return (
         <div>
-        {props.persons.filter(person => 
-            person.name.toLocaleLowerCase().indexOf(props.newSearch.toLowerCase()) !== -1)
-            .map(person => 
-            <h3 key={person.id}>
-                {person.name} 
-                {person.number}
-                <button value={person.id} onClick={props.handleDeletePerson}>delete</button>
-            </h3>
-            )
-        }
+            {props.persons
+                .filter(
+                    (person) =>
+                        person.name
+                            .toLocaleLowerCase()
+                            .indexOf(props.newSearch.toLowerCase()) !== -1
+                )
+                .map((person) => (
+                    <h3 key={person.id}>
+                        {person.name}
+                        {person.number}
+                        <button
+                            value={person.id}
+                            onClick={props.handleDeletePerson}
+                        >
+                            delete
+                        </button>
+                    </h3>
+                ))}
         </div>
     )
 }
